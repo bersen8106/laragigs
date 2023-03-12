8a969808df3c6e6e16a329fe72e819a5db20fce9
bersen8106 <bersen8106@gmail.com>
2023-03-12 17:58:00 +0500

    added partials folder, partial files, Ðpage layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="images/favicon.ico" />
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="icon" href="images/favicon.ico"/>
     <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
@@ -28,7 +28,7 @@
 </head>
 <body class="mb-48">
 <nav class="flex justify-between items-center mb-4">
-    <a href="index.html"
+    <a href="/"
     ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
         /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
@@ -45,7 +45,7 @@
         </li>
     </ul>
 </nav>
-    {{-- VIEW OUTPUT --}}
+{{-- VIEW OUTPUT --}}
 <main>
     @yield('content')
 </main>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,9 @@
 @extends('layout')
 
 @section('content')
+@include('partials._hero')
+@include('partials._search')
+
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 @foreach ($listings as $listing)
             <div class="bg-gray-50 border border-gray-200 rounded p-6">
@@ -12,7 +15,7 @@
                     />
                     <div>
                         <h3 class="text-2xl">
-                            <a href="show.html">{{$listing->title}}</a>
+                            <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
                         </h3>
                         <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                         <ul class="flex">
