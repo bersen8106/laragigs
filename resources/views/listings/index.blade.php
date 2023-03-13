1072308e61df21faf91d0c8b242634758e3592c2
bersen8106 <bersen8106@gmail.com>
2023-03-13 22:02:47 +0500

    using a layout component

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,12 +1,10 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     @include('partials._hero')
     @include('partials._search')
 
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
         @foreach ($listings as $listing)
-            <x-listing-card :listing="$listing" />
+            <x-listing-card :listing="$listing"/>
         @endforeach
     </div>
-@endsection
+</x-layout>
